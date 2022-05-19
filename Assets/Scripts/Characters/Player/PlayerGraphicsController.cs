38e33cc44f50ce3d97169293b0b0eab80a8a92c6
odrmasliuk <odr.masliuk@gmail.com>
2022-05-19 15:00:37 +0200

    fixes

--- Assets/Scripts/Characters/Player/PlayerGraphicsController.cs
+++ Assets/Scripts/Characters/Player/PlayerGraphicsController.cs
@@ -7,7 +7,7 @@ public class PlayerGraphicsController : MonoBehaviour
     [SerializeField] private Animator animator;
     [SerializeField] private SpriteRenderer sprite;
 
-    //[SerializeField] private ParticleSystem stepsPS;
+    [SerializeField] private ParticleSystem stepsPS;
     [SerializeField] private ParticleSystem ghostPS;
 
     [SerializeField] private RuntimeAnimatorController aliveController;
@@ -59,6 +59,9 @@ public class PlayerGraphicsController : MonoBehaviour
     {
         Camera.main.cullingMask = ghostRender;
         ghostPS.Play();
+        stepsPS.Stop();
+
+        SwitchFlashlight(false);
 
         GameObject corpse = Instantiate(new GameObject(), transform.position, Quaternion.identity);
         SpriteRenderer sr = corpse.AddComponent<SpriteRenderer>();
@@ -75,6 +78,7 @@ public class PlayerGraphicsController : MonoBehaviour
     {
         Camera.main.cullingMask = aliveRender;
         ghostPS.Stop();
+        stepsPS.Play();
         animator.runtimeAnimatorController = aliveController;
         animator.SetTrigger("Ghost");
     }
--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using static DialogueSO;
 using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class DialogueManager : MonoBehaviour
 {
